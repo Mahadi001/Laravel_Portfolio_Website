547ffc3a90d926eca460b0a59ed2c9aef665d188
Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-07-07 04:15:28 +0600

    route_and_controller

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PagesController@index')->name('home');
+Route::get('/admin/dashboard', 'PagesController@dashboard')->name('admin.dashboard');
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
